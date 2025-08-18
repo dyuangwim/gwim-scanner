065ef8929d105ddb22ac02b51cf54d6bd5fd5db5
dyuangwim <dyuangwim@gmail.com>
2025-08-19 07:46:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 MYSQL_CONFIG = {
-    "host": "149.28.152.191", # 192.168.20.17
-    "user": "root", # itadmin
-    "password": "fb267146d223afbd24e88d5ea8090b0b8cdc4254146fa62d", # itadmin@2018
-    "database": "production" # production-testing
+    "host": "", 
+    "user": "",
+    "password": "", 
+    "database": "production" # 
 }
 DEVICE_LINE = "HF1" # Change for each production line
 DEVICE_ID = "RPI-01" # Change for each raspberry pi device
+
