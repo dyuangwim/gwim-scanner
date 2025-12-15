32d9a2848665b9a04bdfd09d06f5cf1e2feff87d
dyuangwim <dyuangwim@gmail.com>
2025-12-15 16:41:37 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-MYSQL_CONFIG = {
-    "host": "149.28.152.191", # 192.168.20.17
-    "user": "root", # itadmin
-    "password": "fb267146d223afbd24e88d5ea8090b0b8cdc4254146fa62d", # itadmin@2018
-    "database": "production" # production-testing
-}
-DEVICE_LINE = "HF6" # Change for each production line
-DEVICE_ID = "RPI-01" # Change for each raspberry pi device
